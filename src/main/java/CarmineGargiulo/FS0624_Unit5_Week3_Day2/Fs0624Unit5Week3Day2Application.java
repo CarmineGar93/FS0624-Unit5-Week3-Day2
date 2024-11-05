fdc1e545b66f2cd9478f342cd40dc1ceec4e8ec3
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-05 14:07:20 +0100

    init commit

---
+++ src/main/java/CarmineGargiulo/FS0624_Unit5_Week3_Day2/Fs0624Unit5Week3Day2Application.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.FS0624_Unit5_Week3_Day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs0624Unit5Week3Day2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fs0624Unit5Week3Day2Application.class, args);
+	}
+
+}
---
+++ src/test/java/CarmineGargiulo/FS0624_Unit5_Week3_Day2/Fs0624Unit5Week3Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.FS0624_Unit5_Week3_Day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs0624Unit5Week3Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
